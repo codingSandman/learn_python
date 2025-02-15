8e07d1e80509f1ef9b9f8f8a3a4769d1549f7f3d
rene <100996469+codingSandman@users.noreply.github.com>
2025-02-15 08:17:22 +0100

    translated schaltjahr.py to English

--- schaltjahr.py
+++ schaltjahr.py
@@ -1,12 +1,14 @@
 """
-Ein Schaltjahr ist ein Kalenderjahr, das einen zusätzlichen Schalttag enthält.
-Der zusätzliche Tag (29.02.) tritt in jedem Jahr auf das ein Vielfaches von 4 ist, außer den Jahren, welche Vielfache
-von 100 sind, es sei denn sie sind auch durch 400 teilbar.
+Exercise: 
+A leap year is a calendar year that contains an additional leap day.
+The extra day (29th Feb) occurs in every year that is a multiple of 4, except for years that are multiples of
+of 100, unless they are also divisible by 400.
+date: Feb/2025; Exercise openHPI python course
 """
 
-jahr = 2025
+year = 2025 #Change the year to repeat the check
 
-if jahr % 4 == 0 and (jahr % 100 != 0 or jahr % 400 == 0):
-    print(f"{jahr} ist ein Schaltjahr.")
+if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+    print(f"{year} is a leap year.")
 else:
-    print(f"{jahr} ist kein Schaltjahr.")
+    print(f"{year} is no leap year.")
